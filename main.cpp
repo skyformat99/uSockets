14496b767a01653788a82a1e2cb6ea5e21ddfdc6
Alex Hultman <alexhultman@localhost.localdomain>
2017-04-17 14:22:18 +0200

    Clean up example

--- main.cpp
+++ main.cpp
@@ -7,40 +7,38 @@ int main() {
 
     uS::Berkeley<uS::Epoll> c;
 
-    enum SocketStates {
+    enum States {
         HTTP_SOCKET
     };
 
-    struct HttpState {
+    struct HttpSocket : uS::Berkeley<uS::Epoll>::Socket {
+
+        HttpSocket(uS::Berkeley<uS::Epoll> *context) : uS::Berkeley<uS::Epoll>::Socket(context) {
+            setState(HTTP_SOCKET);
+        }
+
         static void onData(uS::Berkeley<uS::Epoll>::Socket *socket, char *data, size_t length) {
             std::cout << "HttpSocket got data: " << std::string(data, length) << std::endl;
         }
 
         static void onEnd(uS::Berkeley<uS::Epoll>::Socket *socket) {
-            std::cout << "HttpSocket disconnected" << std::endl;
+            HttpSocket *httpSocket = (HttpSocket *) socket;
 
-            socket->close();
+            httpSocket->close();
+            delete httpSocket;
+        }
+
+        static uS::Berkeley<uS::Epoll>::Socket *allocator(uS::Berkeley<uS::Epoll> *context) {
+            return new HttpSocket(context);
         }
     };
-    c.addSocketState<HttpState>(HTTP_SOCKET);
 
-    // application decides how and what to allocate
-    auto socketAllocator = [](uS::Berkeley<uS::Epoll> *context) {
-        return new uS::Berkeley<uS::Epoll>::Socket(context);
-    };
+    c.addSocketState<HttpSocket>(HTTP_SOCKET);
 
-    // symmetric listen and connect
     if (c.listen(nullptr, 3000, 0, [](uS::Berkeley<uS::Epoll>::Socket *socket) {
-
-        // getting nullptr for socket can be sign of error
-
-        std::cout << "Client connected: " << sizeof(*socket) << std::endl;
-
-        // all new sockets needs to have a state set!
-        socket->setState(HTTP_SOCKET);
-
-    }, socketAllocator)) {
-        std::cout << "is now listening" << std::endl;
+        std::cout << "Someone connected" << std::endl;
+    }, HttpSocket::allocator)) {
+        std::cout << "Listening to port 3000" << std::endl;
     }
 
     c.connect("localhost", 3000, [](uS::Berkeley<uS::Epoll>::Socket *socket) {
@@ -48,11 +46,8 @@ int main() {
             std::cout << "Connection failed" << std::endl;
         } else {
             std::cout << "We connected" << std::endl;
-
-            socket->setState(HTTP_SOCKET);
         }
-    }, socketAllocator);
+    }, HttpSocket::allocator);
 
-    // context is the loop
     c.run();
 }
